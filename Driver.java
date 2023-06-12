bdf9c6c7db76ff5fcedc98af293b589f66029b0e
smarthkaul <105581386+smarthkaul@users.noreply.github.com>
2023-06-11 23:16:02 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,26 @@
+import java.io.*;
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
     Polynomial p = new Polynomial();
     System.out.println(p.evaluate(3));
-    double [] c1 = {6,0,0,5};
-    Polynomial p1 = new Polynomial(c1);
-    double [] c2 = {0,-2,0,0,-9};
-    Polynomial p2 = new Polynomial(c2);
+    double [] c1 = {6,5};
+    int[] e1 = {0,3};
+    Polynomial p1 = new Polynomial(c1,e1);
+    double [] c2 = {-2,-9};
+    int[] e2 = {1,4};
+    Polynomial p2 = new Polynomial(c2,e2);
     Polynomial s = p1.add(p2);
+    Polynomial m = p1.multiply(p2);
     System.out.println("s(0.1) = " + s.evaluate(0.1));
-    if(s.hasRoot(1))
-    System.out.println("1 is a root of s");
-    else
-    System.out.println("1 is not a root of s");
+    if(s.hasRoot(1)){
+        System.out.println("1 is a root of s");
+    }
+    else{
+        System.out.println("1 is not a root of s");
+    }
+    System.out.println("p1 * p2 = " + m.evaluate(1));
+    Polynomial p3 = new Polynomial(new File("input.txt"));
+    System.out.println("p3(4) = " + p3.evaluate(4));
+    p3.saveToFile("output.txt");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,30 +1,139 @@
+import java.io.*;
 class Polynomial{
     double polyarr[];
+    int exp[];
+    double coeff[];
     Polynomial(){
-        polyarr = new double[]{0};
+        coeff = new double[]{0};
+        exp = new int[]{0};
     }
-    Polynomial(double arr[]){
-        polyarr = arr;
+    Polynomial(double coeff[], int exp[]){
+        this.coeff = coeff;
+        this.exp = exp;
+    }
+    Polynomial(File f) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String polyString = br.readLine();
+        br.close();
+        String[] polyTerms = polyString.split("(?=[-+])");
+        double[] coeff = new double[polyTerms.length];
+        int[] exp =  new int[polyTerms.length];
+        for(int i = 0; i < polyTerms.length; i++){
+            String polyTerm = polyTerms[i].trim();
+            String parts[] = polyTerm.split("x",2);
+            if(parts.length==1){
+                coeff[i] = Double.parseDouble(parts[0]);
+                exp[i] = 0;
+            }
+            else if(parts.length==2){
+                coeff[i] = Double.parseDouble(parts[0]);
+                exp[i] = Integer.parseInt(parts[1]);
+            }
+        }
+        this.coeff = coeff;
+        this.exp = exp;
+    }
+    void saveToFile(String fileName) throws IOException{
+        String polyWrite = "";
+        for(int i = 0; i < this.coeff.length; i++){
+            if(i != 0){
+                if(this.coeff[i]>0){
+                    polyWrite = polyWrite + "+";
+                }
+            }
+            polyWrite = polyWrite + this.coeff[i];
+            if(this.exp[i] == 0){
+                continue;
+            }
+            polyWrite = polyWrite + "x" + this.exp[i];
+        }
+        FileWriter writer = new FileWriter(fileName);
+        writer.write(polyWrite);
+        writer.close();
+    }
+
+    int findPos(int findExp, int[] exp){
+        for (int i = 0; i < exp.length; i++) {
+            if (findExp == exp[i])
+                return i;
+        }
+        return -1;
+    }
+
+    Polynomial removeZero(double[] coeff, int[] exp){
+        int nonZeroSize = 0;
+        for(int i = 0; i < coeff.length; i++){
+            if(coeff[i] != 0){
+                nonZeroSize++;
+            }
+        }
+        double simplifiedCoeff[] = new double[nonZeroSize];
+        int simplifiedExp[] = new int[nonZeroSize];
+        int index = 0;
+        for(int i = 0; i < coeff.length; i++){
+            if(coeff[i]!=0){
+                simplifiedCoeff[index] = coeff[i];
+                simplifiedExp[index] = exp[i];
+                index++;
+            }
+        }
+        return new Polynomial(simplifiedCoeff, simplifiedExp);
+    }
+
+    Polynomial multiply(Polynomial p){
+        int size1 = this.coeff.length;
+        int size2 = p.coeff.length;
+        double[] multCoeff = new double[size2 * size1];
+        int[] multExp = new int[size2 * size1];
+        int index = 0;
+        for(int i = 0; i < size1; i++){
+            for(int j = 0; j < size2; j++){
+                int pos = findPos((p.exp[j]+this.exp[i]),multExp);
+                if(pos!=-1){
+                    multCoeff[pos] += p.coeff[j]*this.coeff[i];
+                }
+                else{
+                    multCoeff[index] = this.coeff[i]*p.coeff[j];
+                    multExp[index] = this.exp[i] + p.exp[j];
+                    index++;
+                }
+            }
+        }
+        return removeZero(multCoeff, multExp);
     }
     Polynomial add(Polynomial p){
-        int length = Math.max(p.polyarr.length, this.polyarr.length);
-        double sum[] = new double[length];
-        for(int i = 0; i < polyarr.length; i++){
-            sum[i] = polyarr[i];
+        int size1 = this.coeff.length;
+        int size2 = p.coeff.length;
+        double[] addCoeff = new double[size2 + size1];
+        int[] addExp = new int[size2 + size1];
+        int index = 0;
+        for(int i = 0; i < size1; i++){
+            addCoeff[index] = this.coeff[i];
+            addExp[index] = this.exp[i];
+            index++;
         }
-        for(int i = 0; i < p.polyarr.length; i++){
-            sum[i] += p.polyarr[i];
+        for(int i = 0; i < size2; i++){
+            int pos = findPos(p.exp[i], addExp);
+            if(pos != -1){
+                addCoeff[pos] += p.coeff[i];
+            }
+            else{
+                addCoeff[index] = p.coeff[i];
+                addExp[index] = p.exp[i];
+                index++;
+            }
         }
-        Polynomial ans = new Polynomial(sum);
-        return ans;
+        return removeZero(addCoeff, addExp);
     }
+
     double evaluate(double val){
         double sum = 0;
-        for(int i = 0; i < polyarr.length; i++){
-            sum += polyarr[i]*Math.pow(val,i);
+        for(int i = 0; i < coeff.length; i++){
+            sum += coeff[i]*Math.pow(val,exp[i]);
         }
         return sum;
     }
+    
     boolean hasRoot(double val){
         return evaluate(val)==0;
     }
